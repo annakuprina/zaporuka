d7a8d0ee51435d8e4612c82d50e509ed3c2d5f3d
Alina <alinaskripets92@gmail.com>
2020-02-11 19:04:22 +0200

    change wp config file

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'zaporuka' );
+define( 'DB_NAME', 'sampin_wp382' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'sampin_wp382' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '238DS)x.Qp' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
