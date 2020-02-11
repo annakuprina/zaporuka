6ae88aab802696af859a4f0ee0a55bfd3507c72e
Alina <alinaskripets92@gmail.com>
2020-02-11 14:53:53 +0200

    styling adaptivity of sliders tabs

--- wp-content/themes/u-design-child/assets/js/main.js
+++ wp-content/themes/u-design-child/assets/js/main.js
@@ -23,24 +23,6 @@ jQuery(document).ready(function($) {
     slidesToScroll: 1,
     arrows: false,
     dots: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-    ]
   });
 
   // VOLUNTEERS SLIDER DESKTOP
@@ -49,24 +31,6 @@ jQuery(document).ready(function($) {
     slidesToScroll: 1,
     arrows: false,
     dots: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-    ]
   });
 
   // PHOTO SLIDER
@@ -106,7 +70,25 @@ jQuery(document).ready(function($) {
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: false,
-    dots: true
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
   // lightbox for video slider
   $(".video-slider").slickLightbox({});
@@ -116,7 +98,25 @@ jQuery(document).ready(function($) {
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: false,
-    dots: true
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
 
   // lightbox for documents slider
@@ -125,18 +125,17 @@ jQuery(document).ready(function($) {
     itemSelector: ".documents-slide > img"
   });
 
-    $('.slide').slick({
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      //adaptiveHeight: true
-    });
+  $('.slide').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    //adaptiveHeight: true
+  });
 
 
-    // refresh sliders after click on tab
-    $('.one-tab-link').click(function(){
-       $(".slick-media-slider").slick('refresh');
-    });
-
+  // refresh sliders after click on tab
+  $('.one-tab-link').click(function(){
+      $(".slick-media-slider").slick('refresh');
+  });
 });
