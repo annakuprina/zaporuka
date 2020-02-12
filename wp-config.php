48f005afc7025af0872a95d50cec761d6925f853
annakuprina <web.anna.kuprina@gmail.com>
2020-02-12 12:20:29 +0200

    fake count field css and fix js

--- wp-config.php
+++ wp-config.php
@@ -81,6 +81,7 @@ $table_prefix = 'wp4t_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'WP_DEBUG', false );
+define( 'WP_DEBUG_DISPLAY ', false );
 //define( 'WP_DEBUG', true );
 //define( 'WP_DEBUG_DISPLAY ', true );
 
