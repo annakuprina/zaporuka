523c4f92d5bcc9c5566976093f67fc4a0aa22c03
annakuprina <web.anna.kuprina@gmail.com>
2020-02-18 18:12:48 +0200

    Canceled payment notice (fix errors)

--- wp-content/themes/u-design-child/woocommerce/checkout/thankyou.php
+++ wp-content/themes/u-design-child/woocommerce/checkout/thankyou.php
@@ -45,11 +45,11 @@ defined( 'ABSPATH' ) || exit;
 				    $notice_pending =  'Ви скасували оплату замовлення';
 				}
 				elseif(ICL_LANGUAGE_CODE=='en'){
-				    $notice_pending =  'You have canceled payment';
+				   $notice_pending =  'You have canceled payment';
 				}
+				echo $notice_pending;
 				?>
 			</p>
-		<?php endif; ?>
 		<?php else : ?>
 
 			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
