c4f58e39d8aec205ed19a8865d1bdf84e474f638
@yana.zagrebelnaya91 <yana.zagrebelnaya91@gmail.com>
2020-02-18 16:46:12 +0200

    changes for review section

--- wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
+++ wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
@@ -64,15 +64,15 @@ jQuery(document).ready(function($) {
     slidesToScroll: 3
   });
 
-  // $('.reviews-block').on('click', '.pagination a', function(e){
-  //   e.preventDefault();
-  //   var link = $(this).attr('href');
-  //   $('.reviews-block').fadeOut(500, function(){
-  //     $(this).load(link + ' .reviews-block', function() {
-  //       $(this).fadeIn(500);
-  //     });
-  //   });
-  // });
+  $('.reviews-block').on('click', '.pagination-block a', function(e){
+    e.preventDefault();
+    var link = $(this).attr('href');
+    $('.reviews-wrapper').fadeOut(100, function(){
+      $(this).load(link + ' .reviews-wrapper', function() {
+        $(this).fadeIn(100);
+      });
+    });
+  });
 
 
 });
