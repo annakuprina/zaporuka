6c3f5167cea72dea423f021dbd7d7c63266e4ddf
@yana.zagrebelnaya91 <yana.zagrebelnaya91@gmail.com>
2020-02-19 10:31:21 +0200

    changes for review section

--- wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
+++ wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
@@ -1,19 +1,12 @@
 jQuery(document).ready(function($) {
   $(function() {
     $(".one-tab-link").on("click", function() {
-      console.log("tab is clicked");
       $(this).addClass("tab-active");
-
       var clickedTitleDataId = $(this).attr("data-id"); //get data-id attribute of clicked title
-      console.log("clicked" + clickedTitleDataId);
       /*Find tab-content with same data-id attribute like clicked title*/
       $(".tabs_content").each(function() {
         var tabsContentDataId = $(this).attr("data-id");
-        console.log("we have content with id " + tabsContentDataId);
-
         if (clickedTitleDataId == tabsContentDataId) {
-          console.log("the same like " + clickedTitleDataId);
-
           $(".tabs_content").removeClass("active");
           $(this).addClass("active");
         }
@@ -64,15 +57,92 @@ jQuery(document).ready(function($) {
     slidesToScroll: 3
   });
 
-  $('.reviews-block').on('click', '.pagination-block a', function(e){
-    e.preventDefault();
-    $('.page-numbers').removeClass('current');
-    $(this).addClass('current');
-    var link = $(this).attr('href');
-    $('.reviews-wrapper').fadeOut(0, function(){
-      $(this).load(link + ' .reviews-wrapper', function() {
-        $(this).fadeIn(0);
-      });
+  // $('.reviews-block').on('click', '.pagination-block a', function(e){
+  //   e.preventDefault();
+  //   $('.page-numbers').removeClass('current');
+  //   $(this).addClass('current');
+  //   var link = $(this).attr('href');
+  //   $('.reviews-wrapper').fadeOut(0, function(){
+  //     $(this).load(link + ' .reviews-wrapper', function() {
+  //       $(this).fadeIn(0);
+  //     });
+  //   });
+  // });
+  if( $( ".pagination-block" ).length ) {
+    $( ".pagination-block" ).wpPagination();
+  }
+  $.fn.wpPagination = function( options ) {
+    options = $.extend({
+      links: "a",
+      action: "my_pagination",
+      ajaxURL: "http://" + location.host + "/wp-admin/admin-ajax.php",
+      next: ".next",
+      previous: ".previous",
+      disablePreviousNext: true
+    }, options);
+
+    function WPPagination( element ) {
+      this.$el = $( element );
+      this.init();
+    }
+
+    WPPagination.prototype = {
+      init: function() {
+        this.createLoader();
+        this.handlePreviousNextLinks();
+        this.handleLinks();
+      },
+      createLoader: function() {
+        var self = this;
+        self.$el.before( "<div id='pagination-loader'></div>" );
+      },
+      handlePreviousNextLinks: function() {
+        var self = this;
+        var $previous = $( options.previous, self.$el );
+        var $next = $( options.next, self.$el );
+
+        if( options.disablePreviousNext ) {
+          $previous.remove();
+          $next.remove();
+        } else {
+          $previous.addClass( "clicked" );
+          $next.addClass( "clicked" );
+        }
+      },
+      handleLinks: function() {
+        var self = this,
+            $links = $( options.links, self.$el ),
+            $loader = $( "#pagination-loader" );
+
+        $links.click(function( e ) {
+          e.preventDefault();
+          var $a = $( this ),
+              url = $a.attr( "href" ),
+              page = url.match( /\d+/ ), // Get the page number
+              pageNumber = page[0],
+              data = {
+                action: options.action, // Pass the AJAX action name along with the page number
+                page: pageNumber
+              };
+
+          if( !$a.hasClass( "clicked" ) ) { // We don't want duplicated posts
+
+            $loader.show(); // Show the loader
+
+            $.get( options.ajaxURL, data, function( html ) {
+              $loader.hide(); // Hide the loader
+              $loader.before( html ); // Insert posts
+              $a.addClass( "clicked" ); // Flag the current link as clicked
+            });
+
+          }
+        });
+      }
+    };
+
+    return this.each(function() {
+      var element = this;
+      var pagination = new WPPagination( element );
     });
-  });
+  };
 });
