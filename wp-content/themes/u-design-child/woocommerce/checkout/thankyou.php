2cabd946b14f72b3cffda01e3b83adb26e8aa247
annakuprina <web.anna.kuprina@gmail.com>
2020-02-24 16:14:11 +0200

    add page refresh is status pending

--- wp-content/themes/u-design-child/woocommerce/checkout/thankyou.php
+++ wp-content/themes/u-design-child/woocommerce/checkout/thankyou.php
@@ -40,6 +40,7 @@ defined( 'ABSPATH' ) || exit;
 		<?php else : ?>
 
 			<?php if( $order->get_status() != 'pending'):?>
+				<?php header("Refresh:0");?>
 
 				<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
