0094b63adf48d72032b3cab6a8a5a187da9ebfd1
@yana.zagrebelnaya91 <yana.zagrebelnaya91@gmail.com>
2020-02-28 16:39:00 +0200

    changes for reports section

--- wp-content/themes/u-design-child/inc/custom_functions.php
+++ wp-content/themes/u-design-child/inc/custom_functions.php
@@ -97,10 +97,9 @@ if($_POST['show_children'] == 'true'){
     $cptaQuery = new WP_Query( $args );
     $html = "";
     if( isset($cptaQuery->posts) ){ ?>
-        <div class='children-block'>
-            <div class='children-wrapper'>
-                <div class="preloader"></div>
-                <?php for($i=0;$i<sizeof($cptaQuery->posts);++$i){
+        <div class='children-wrapper'>
+            <div class="preloader"></div>
+            <?php for($i=0;$i<sizeof($cptaQuery->posts);++$i){
                     $post_id = $cptaQuery->posts[$i]->ID;
                     $thumbnail = get_the_post_thumbnail_url($post_id);
                     $child_age = get_post_meta($post_id, 'child_age', true);
@@ -132,45 +131,44 @@ if($_POST['show_children'] == 'true'){
                         </div>
                     </div><!-- end one-child  -->
                 <?php } ?>
-            </div>
-            <?php wp_reset_postdata(); ?>
-            <div class="pagination-children">
-                <?php
-                $cpta_args = array('posts_per_page' => -1,'post_type' => $cptaType,'post_status' => 'publish');
-                $cpta_Query = new WP_Query( $cpta_args );
-                $cpta_Count = count($cpta_Query->posts);
-                $cpta_Paginationlist = ceil($cpta_Count/$cptaLimit);
-                $last = ceil( $cpta_Paginationlist );
-                $adjacents = "2";
-                if ( $paged == 1 ) {$prev = 1;} else{$prev = $paged - 1;}
-                if ( $paged == $last ) {$next = $last;} else{$next = $paged + 1;}
-                if( $cpta_Paginationlist > 0 ){ ?>
-                    <ul class='list-cptapagination'>
-                        <li class='pagitext'><a href='' class='step-backward step-arrow' data-cpta='1' data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></a></li>
-                        <li class='pagitext'><a href='' class='step-prev step-arrow' data-prev="prev" data-cpta="<?php echo $prev; ?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></i></a></li>
-                        <?php
-                        $t = ceil($paged/6 )-1;
-                        $t2 = $t*6;
-                        for( $cpta=$t2+1; $cpta <= $t2+6; $cpta++){
-                            if ( $cpta > $last ) {
-                                continue;
-                            }
-                            if( $cpta == $paged ){ $active="active_review"; }else{ $active=""; }
-                            ?>
-                            <li><a href='' id='post' class="<?php echo $active;?>" data-cpta="<?php echo $cpta;?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"><?php echo $cpta;?></a></li>
-                        <?php } ?>
-                        <li class='pagitext'><a href='' class='step-next step-arrow' data-cpta="<?php echo $next; ?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></a></li>
-                        <li class='pagitext'><a href='' class='step-forward step-arrow' data-cpta="<?php echo $last;?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></a></li>
-                    </ul>
-                    <?php if ( ($t2+1) == $last ) { ?>
-                        <div class="count_pages"><span class="paged_review"><?php echo $last; ?></span> сторiнка з <span class="paged_review"><?php echo $last; ?></span></div>
-                    <?php } else if ( ($last - (($t2+1))) < 6 ) { ?>
-                        <div class="count_pages"><span class="paged_review"><?php echo ($t2+1) . '-' . $last; ?></span> сторiнки з <span class="paged_review"><?php echo $last; ?></span></div>
-                    <?php } else {?>
-                        <div class="count_pages"><span class="paged_review"><?php echo ($t2+1) . '-' . ($t2+6); ?></span> сторiнки з <span class="paged_review"><?php echo $last; ?></span></div>
-                    <?php  } ?>
+        </div>
+        <?php wp_reset_postdata(); ?>
+        <div class="pagination-children">
+            <?php
+            $cpta_args = array('posts_per_page' => -1,'post_type' => $cptaType,'post_status' => 'publish');
+            $cpta_Query = new WP_Query( $cpta_args );
+            $cpta_Count = count($cpta_Query->posts);
+            $cpta_Paginationlist = ceil($cpta_Count/$cptaLimit);
+            $last = ceil( $cpta_Paginationlist );
+            $adjacents = "2";
+            if ( $paged == 1 ) {$prev = 1;} else{$prev = $paged - 1;}
+            if ( $paged == $last ) {$next = $last;} else{$next = $paged + 1;}
+            if( $cpta_Paginationlist > 0 ){ ?>
+                <ul class='list-cptapagination'>
+                    <li class='pagitext'><a href='' class='step-backward step-arrow' data-cpta='1' data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></a></li>
+                    <li class='pagitext'><a href='' class='step-prev step-arrow' data-prev="prev" data-cpta="<?php echo $prev; ?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></i></a></li>
+                    <?php
+                    $t = ceil($paged/6 )-1;
+                    $t2 = $t*6;
+                    for( $cpta=$t2+1; $cpta <= $t2+6; $cpta++){
+                        if ( $cpta > $last ) {
+                            continue;
+                        }
+                        if( $cpta == $paged ){ $active="active_review"; }else{ $active=""; }
+                        ?>
+                        <li><a href='' id='post' class="<?php echo $active;?>" data-cpta="<?php echo $cpta;?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"><?php echo $cpta;?></a></li>
+                    <?php } ?>
+                    <li class='pagitext'><a href='' class='step-next step-arrow' data-cpta="<?php echo $next; ?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></a></li>
+                    <li class='pagitext'><a href='' class='step-forward step-arrow' data-cpta="<?php echo $last;?>" data-limit="<?php echo $cptaLimit; ?>" data-type="<?php echo $cptaType; ?>"></a></li>
+                </ul>
+                <?php if ( ($t2+1) == $last ) { ?>
+                    <div class="count_pages"><span class="paged_review"><?php echo $last; ?></span> сторiнка з <span class="paged_review"><?php echo $last; ?></span></div>
+                <?php } else if ( ($last - (($t2+1))) < 6 ) { ?>
+                    <div class="count_pages"><span class="paged_review"><?php echo ($t2+1) . '-' . $last; ?></span> сторiнки з <span class="paged_review"><?php echo $last; ?></span></div>
+                <?php } else {?>
+                    <div class="count_pages"><span class="paged_review"><?php echo ($t2+1) . '-' . ($t2+6); ?></span> сторiнки з <span class="paged_review"><?php echo $last; ?></span></div>
                 <?php } ?>
-            </div>
+            <?php } ?>
         </div>
         <?php
         die();
