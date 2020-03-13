2ead6b15972bd725c777d2ecee6ab1d91d734a48
YanaZagrebelna <yana.zagrebelnaya91@gmail.com>
2020-03-13 18:03:24 +0200

    cron update history payments

--- wp-content/plugins/liqpay_wordpress/liqpay-answer.php
+++ wp-content/plugins/liqpay_wordpress/liqpay-answer.php
@@ -52,7 +52,7 @@ function insert_history($project_id2, $transaction_id2, $date2, $users_name2, $u
 
 }
 
-update_option('payment_answer_from_liqpay', $_POST);
+update_option('payment_answer_from_liqpay', $_POST['data']);
 if (isset($_POST['data'])) {
     $json = base64_decode($_POST['data']);
     $obj = json_decode($json);
