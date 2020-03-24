f072c2bf6502b6d847294df73c7ccf137d366fbb
Yana Zagrebelna <web.yana.zagrebelna@gmail.com>
2020-03-24 15:59:52 +0200

    payments update

--- wp-content/themes/u-design-child/test-page.php
+++ wp-content/themes/u-design-child/test-page.php
@@ -2,3 +2,15 @@
 /**
  * Template Name: Page Test
  */
+$public_key = get_option('liqpay_merchant_id');
+$private_key = get_option('liqpay_signature_id');
+$date_from = date(strtotime('-1 day') )* 1000;
+$date_to = time() * 1000;
+$liqpay = new MyLiqPay($public_key, $private_key);
+$res = $liqpay->api("request", array(
+    'action'    => 'reports',
+    'version'   => '3',
+    'date_from' => $date_from,
+    'date_to'   => $date_to
+));
+var_dump($res);
