e60a710786e23fc41b11bf59b8bb53bdf2b9b8d4
Alina <alinaskripets92@gmail.com>
2020-03-25 16:50:24 +0200

    adaptivity changes

--- wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
+++ wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
@@ -60,23 +60,23 @@ jQuery(document).ready(function($) {
     ]
   });
 
-  $(".write_about_us_carousel .vc_pageable-slide-wrapper").slick({
-    dots: true,
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      }
-    ]
-  });
+  // $(".write_about_us_carousel .vc_pageable-slide-wrapper").slick({
+  //   dots: true,
+  //   arrows: false,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 3,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     }
+  //   ]
+  // });
 
   $(".other-projects-single-project .vc_pageable-slide-wrapper").slick({
     dots: true,
