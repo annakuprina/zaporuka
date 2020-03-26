631cc4c49dda242819f56b263034eef42b8a51ae
Yana Zagrebelna <web.yana.zagrebelna@gmail.com>
2020-03-26 15:19:52 +0200

    categories update

--- wp-content/themes/u-design-child/inc/shortcodes.php
+++ wp-content/themes/u-design-child/inc/shortcodes.php
@@ -134,7 +134,7 @@ $back_button = !empty($options['404_back_main_' . ICL_LANGUAGE_CODE]) ? $options
                     ));
 
                     if( $res->result == 'ok' ) {
-                        wpdb_update( $table_liqpay, [ 'status'=>'unsubscribe' ], [ 'unsubscribe'=>$_GET['order_id'] ] );
+                        wpdb_update( $table_liqpay, [ 'status'=>'unsubscribe' ], [ 'order_id'=>$_GET['order_id'] ] );
                     }
                     $message = !empty($options['cancel_subscription_text_after_' . ICL_LANGUAGE_CODE]) ? $options['cancel_subscription_text_after_' . ICL_LANGUAGE_CODE] : false;
                 } else{
