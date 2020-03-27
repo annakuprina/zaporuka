2208215ed925a18da9ca89aa5262a554f0bfd78c
Alina <alinaskripets92@gmail.com>
2020-03-27 21:00:54 +0200

    one project page changes

--- wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
+++ wp-content/themes/u-design-child/assets/js/custom_scrypt_yana.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function($) {
+
+  // One project page. Tabs.
   $(function() {
     $(".one-tab-link").on("click", function() {
       $(".one-tab-link").removeClass("tab-active");
@@ -20,32 +22,32 @@ jQuery(document).ready(function($) {
     arrows: false,
     slidesToShow: 3,
     slidesToScroll: 3,
+    infinite: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          infinite: true,
-          dots: true
         }
       }
     ]
   });
 
+
+  // One project page. Project partners carousel.
   $(".project-partners-carousel .vc_pageable-slide-wrapper").slick({
     dots: true,
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4,
+    infinite: true,
     responsive: [
       {
         breakpoint: 1200,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true,
-          dots: true
         }
       },
       {
@@ -53,39 +55,56 @@ jQuery(document).ready(function($) {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          infinite: true,
-          dots: true
         }
       }
     ]
   });
 
+
+  /*About us page. Carousel with news.*/ 
   $(".write_about_us_carousel .write-about-us-slick-desk").slick({
     dots: true,
     arrows: false,
     slidesToShow: 3,
     slidesToScroll: 3,
+    infinite: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          infinite: true,
-          dots: true
         }
       }
     ]
   });
 
+  /*One Project page."Other projects" slider */
   $(".other-projects-single-project .vc_pageable-slide-wrapper").slick({
     dots: true,
     arrows: false,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 
-  /*About us page / Block "what we do" With projects*/
+  /*About Us page .Block "What we do" (with projects blocks)*/
   $(".about-us-project-section .vc_pageable-slide-wrapper").slick({
     dots: true,
     arrows: false,
@@ -169,6 +188,8 @@ jQuery(document).ready(function($) {
     slidesToScroll: 1
   });
 
+
+  // One project progress bar width
   $(window).bind( 'grid:items:added', function(){
     $(".other-projects-row .vc_grid-item").each(function() {
       var moneyTotalAmount = $(this)
