96d92aa6e9376edfa249866495a81dcb82bed0ea
Yana Zagrebelna <web.yana.zagrebelna@gmail.com>
2020-04-01 12:15:31 +0300

    home page changes

--- wp-content/themes/u-design-child/functions.php
+++ wp-content/themes/u-design-child/functions.php
@@ -141,7 +141,7 @@ add_action('init', function() {
   pll_register_string('u-design-child', 'Дивитись вiдео');
   pll_register_string('u-design-child', 'Долучайтесь до БФ Запорука у соцмережах');
   pll_register_string('u-design-child', 'Партнери');
-  pll_register_string('u-design-child', 'Друзi');
+  pll_register_string('u-design-child', 'Партнери та друзі фонду');
   pll_register_string('u-design-child', 'Волонтери');
   pll_register_string('u-design-child', 'Дякуємо за ваше добро!');
   pll_register_string('u-design-child', 'Ви скасували оплату!');
