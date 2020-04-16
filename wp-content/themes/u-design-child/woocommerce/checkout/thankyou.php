e735a9b911742eb8e4de7180401992f25956445c
Yana Zagrebelna <web.yana.zagrebelna@gmail.com>
2020-04-16 19:42:55 +0300

    fix subscriptions

--- wp-content/themes/u-design-child/woocommerce/checkout/thankyou.php
+++ wp-content/themes/u-design-child/woocommerce/checkout/thankyou.php
@@ -37,7 +37,7 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php else : ?>
 
-			<?php if( $order->get_status() != 'pending' || $order->get_status() == 'completed'):?>
+			<?php if( $order->get_status() == 'completed' || $order->get_status() != 'pending'):?>
 
 				<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
