2b8f77bb39216dec82a1ad1c3967032235321048
annakuprina <34443711+annakuprina@users.noreply.github.com>
2020-04-26 19:10:53 +0300

    name in email

--- wp-content/themes/u-design-child/woocommerce/emails/customer-completed-order.php
+++ wp-content/themes/u-design-child/woocommerce/emails/customer-completed-order.php
@@ -28,7 +28,7 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 
 <?php /* translators: %s: Customer first name */ ?>
-<p><?php printf( pll_e( 'Вітаємо'), esc_html( $order->get_billing_first_name() ) ); ?></p>
+<p><?php  pll_e( 'Вітаємо'); echo ", " . $order->get_billing_first_name() ; ?></p>
 <?php /* translators: %s: Site title */ ?>
 
 <p><?php pll_e( 'Ми закінчили обробляти ваше замовлення.'); ?></p>
