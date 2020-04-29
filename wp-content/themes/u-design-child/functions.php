5e050d1f6dc2a84e90d442a7f667b6cf23600d98
Yana Zagrebelna <web.yana.zagrebelna@gmail.com>
2020-04-29 15:47:06 +0300

    fix email template

--- wp-content/themes/u-design-child/functions.php
+++ wp-content/themes/u-design-child/functions.php
@@ -425,13 +425,13 @@ function createOrder_ru_address($order){
             $order->set_billing_state($npArea['description']);
             $order->update_meta_data('wc_ukr_shipping_np_area_ref', esc_attr($_POST[WC_UKR_SHIPPING_NP_SHIPPING_NAME . '_area']));
         }
-        $npCity = $wpdb->get_row("SELECT description_ru FROM wc_ukr_shipping_np_cities WHERE area_ref = '" . esc_attr($_POST[WC_UKR_SHIPPING_NP_SHIPPING_NAME . '_city']) . "'", ARRAY_A);
+        $npCity = $wpdb->get_row("SELECT description_ru FROM wc_ukr_shipping_np_cities WHERE ref = '" . esc_attr($_POST[WC_UKR_SHIPPING_NP_SHIPPING_NAME . '_city']) . "'", ARRAY_A);
         if ($npCity) {
             $order->set_shipping_city($npCity['description']);
             $order->set_billing_city($npCity['description']);
             $order->update_meta_data('wc_ukr_shipping_np_city_ref', esc_attr($_POST[WC_UKR_SHIPPING_NP_SHIPPING_NAME . '_city']));
         }
-        $npWarehouse = $wpdb->get_row("SELECT description_ru FROM wc_ukr_shipping_np_warehouses WHERE city_ref = '" . esc_attr($_POST[WC_UKR_SHIPPING_NP_SHIPPING_NAME . '_warehouse']) . "'", ARRAY_A);
+        $npWarehouse = $wpdb->get_row("SELECT description_ru FROM wc_ukr_shipping_np_warehouses WHERE ref = '" . esc_attr($_POST[WC_UKR_SHIPPING_NP_SHIPPING_NAME . '_warehouse']) . "'", ARRAY_A);
         if ($npWarehouse) {
             $order->set_shipping_address_1($npWarehouse['description']);
             $order->set_billing_address_1($npWarehouse['description']);
